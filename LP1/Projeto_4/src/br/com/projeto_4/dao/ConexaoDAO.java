66d8949a960880ebacc32ad219e61f886da8caa8
Marcelo-Gallo <129621653+Marcelo-Gallo@users.noreply.github.com>
2024-07-26 21:18:11 -0300

    Update ConexaoDAO.java

--- LP1/Projeto_4/src/br/com/projeto_4/dao/ConexaoDAO.java
+++ LP1/Projeto_4/src/br/com/projeto_4/dao/ConexaoDAO.java
@@ -15,9 +15,9 @@ public class ConexaoDAO {
     public static void ConnectDB() {
         try {
             //Dados para conectar com o banco de dados Postgres
-            String dsn = "projeto_4"; //nome do banco de dados
-            String user = "postgres"; //nome do usuario utilizado para se conectar
-            String senha = "postdba"; //senha do usuario acima informado
+            String dsn = "###"; //nome do banco de dados
+            String user = "###"; //nome do usuario utilizado para se conectar
+            String senha = "###"; //senha do usuario acima informado
 
             DriverManager.registerDriver(new org.postgresql.Driver());
 
